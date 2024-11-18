25ea7b82b01d374a99e065ff90922d78600ce674
vitormarinheiro1 <vitormarinheiro9@gmail.com>
2024-11-18 11:16:49 -0300

    fix: removendo arquivos não utilizados

--- views/inicio-html.php
+++ views/inicio-html.php
@@ -24,7 +24,6 @@
 
             <div class="cabecalho__icones">
                 <a href="/novo-video" class="cabecalho__videos"></a>
-                <a href="../pages/login.html" class="cabecalho__sair">Sair</a>
             </div>
         </nav>
 
