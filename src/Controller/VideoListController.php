ca0fb4d6966e14811af2545df837af2bd7b774b9
vitormarinheiro1 <vitormarinheiro9@gmail.com>
2024-11-18 11:08:37 -0300

    feat: add views

--- src/Controller/VideoListController.php
+++ src/Controller/VideoListController.php
@@ -9,29 +9,11 @@ use Alura\Mvc\Repository\VideoRepository;
 class VideoListController
 {
     public function __construct(private VideoRepository $videoRepository)
-    {}
+    {
+    }
     public function processaRequisicao(): void
     {
         $videoList = $this->videoRepository->all();
-        require_once __DIR__ . '/../../inicio-html.php';
-        ?>
-        <ul class="videos__container" alt="videos alura">
-            <?php foreach ($videoList as $video): ?>
-                <li class="videos__item">
-                    <iframe width="100%" height="72%" src="<?= $video->url ?>" title="YouTube video player" frameborder="0"
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                        allowfullscreen></iframe>
-                    <div class="descricao-video">
-                        <img src="./img/logo.png" alt="logo canal alura">
-                        <h3><?= $video->title ?></h3>
-                        <div class="acoes-video">
-                            <a href="/editar-video?id=<?= $video->id; ?>">Editar</a>
-                            <a href="/remover-video?id=<?= $video->id; ?>">Excluir</a>
-                        </div>
-                    </div>
-                </li>
-            <?php endforeach; ?>
-        </ul>
-        <?php require_once __DIR__ . '/../../fim-html.php';
+        require_once __DIR__ . "/../../views/video-list.php";
     }
 }
