07201e991d8cab23a1fe5df458fea636b4925cca
vitormarinheiro1 <vitormarinheiro9@gmail.com>
2024-11-18 13:32:09 -0300

    fix: correção de rotas

--- config/routes.php
+++ config/routes.php
@@ -1,17 +1,20 @@
 <?php
 
-declare(strict_types= 1);
+declare(strict_types=1);
 
-use Alura\Mvc\Controller\DeleteVideoController;
-use Alura\Mvc\Controller\EditVideoController;
-use Alura\Mvc\Controller\NewVideoController;
-use Alura\Mvc\Controller\VideoListController;
+use Alura\Mvc\Controller\{
+    DeleteVideoController,
+    EditVideoController,
+    NewVideoController,
+    VideoFormController,
+    VideoListController
+};
 
 return [
-    "GET/" => VideoListController::class,
-    "GET/novo-video" => VideoListController::class,
-    "POST/novo-video" => NewVideoController::class,
-    "GET/editar-video" => VideoListController::class,
-    "POST/editar-video" => EditVideoController::class,
-    "GET/remover-video" => DeleteVideoController::class,
+    "GET|/" => VideoListController::class,
+    "GET|/novo-video" => VideoFormController::class,
+    "POST|/novo-video" => NewVideoController::class,
+    "GET|/editar-video" => VideoFormController::class,
+    "POST|/editar-video" => EditVideoController::class,
+    "GET|/remover-video" => DeleteVideoController::class,
 ];
